c3dddcab549727f2b5582814c0f40ca088aaac1a
tuanpx-dev <tuanpx.dev@gmail.com>
2019-12-19 17:13:39 +0700

    init module user

---
+++ travel-be/apps/users/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from apps.users import models as user_models
+
+
+class LoginEmailValidator(serializers.Serializer):
+    email = serializers.EmailField(required=True, allow_blank=False)
+    password = serializers.CharField(required=True, allow_blank=False)
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = user_models.User
+        fields = ('id', 'username', 'display_name', 'email', 'age',
+                  'place', 'created_at', 'created_at', 'updated_at')
+
+
+class TokenSerializer(serializers.Serializer):
+    access_token = serializers.CharField()
+    expired_time = serializers.DateTimeField()
+    user = UserSerializer(read_only=True)
